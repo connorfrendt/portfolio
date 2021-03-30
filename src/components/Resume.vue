fbb875e1d80ca439bb3784c98e698d66edcf0260
Connor Frendt <cjacksonfx@yahoo.com>
2021-03-30 16:38:17 -0700

    wip getting buttons to display different info in resume

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -2,12 +2,12 @@
   <div>
         <h3>EXPERIENCE</h3>
 
-            <button id="collapsible" @click="collapsible">
+            <button @click="collapsible()" id="hey">
                 Database Developer | Pollinate | Portland, OR
                     <br />
                 December 2019 - August 2020
             </button>
-                <div id="content">
+                <div v-show="clicked">
                     As a Database Developer, I was transitioning to a software engineer within the company.  I assisted with software development projects for clients like Carhartt and Life is Good, as well as maintained their databases.  I was responsible for making data that's human readable to get output to the front-end.
                     I manipulated the data behind 100+ Under Armour garments per month, as well as helped out with the data for JBL speakers, Astro Headsets, and Bauer Hockey equipment, among others.
                 </div>
@@ -19,19 +19,24 @@
                 </p>
 
         <h3>TECHNICAL SKILLS</h3>
-            Languages:  HTML, CSS, Javascript, Python, SQL
-            <br />
-            Front End:  Vanilla JS, Vue.js, CSS Grid, CSS Flexbox
-            <br />
-            Back End: Node.js, Express.js
-            <br />
-            Databases: SQL, PostgreSQL, Mongoose/Mongo
-            <br />
-            Testing Suites: Jest, TDD
-            <br />
-            Tools: Git, GitHub, Slack, Jira, Atlassian, BitBucket
-            <br />
-            Other: REST APIs, Responsive Web Design, Agile, OAuth, JSON Web Token
+            <button>
+                Tech Stacks/Languages
+            </button>
+            <div v-show="clicked">
+                Languages:  HTML, CSS, Javascript, Python, SQL
+                <br />
+                Front End:  Vanilla JS, Vue.js, CSS Grid, CSS Flexbox
+                <br />
+                Back End: Node.js, Express.js
+                <br />
+                Databases: SQL, PostgreSQL, Mongoose/Mongo
+                <br />
+                Testing Suites: Jest, TDD
+                <br />
+                Tools: Git, GitHub, Slack, Jira, Atlassian, BitBucket
+                <br />
+                Other: REST APIs, Responsive Web Design, Agile, OAuth, JSON Web Token
+            </div>
 
 
         <h3>EDUCATION</h3>
@@ -39,9 +44,17 @@
                 <li v-for="school in schools" :key="school.id">
                     <div>{{ school.program }} | {{ school.gradDate }}</div>
                     <div>{{ school.institution }} | {{ school.place }}</div>
+                    <div>{{ school.description }}</div>
                 </li>  
             </ul>
 
+        <h3>VOLUNTEER</h3>
+            <ul>
+                <li v-for="volunteer in volunteering" :key="volunteer.id">
+                    <div>{{ volunteer.company }} | {{ volunteer.place }}</div>
+                    <div>{{ volunteer.description }}</div>
+                </li>
+            </ul>
     </div>
 </template>
 
@@ -49,28 +62,41 @@
 export default {
     data() {
         return {
+            clicked: false,
             schools: [
                 {
                     id: 'wwu',
                     institution: 'Western Washington University',
                     gradDate: 'Dec 2017',
                     program: 'B.S. in Mathematics',
-                    place: 'Bellingham, WA'
+                    place: 'Bellingham, WA',
+                    description: '5 years of single and multivariable calculus, with linear algebra, systems of partial differential equations, and forier analysis.'
                 },
                 {
                     id: 'alchemy',
                     institution: 'Alchemy Code Lab',
                     gradDate: 'Feb 2019',
                     program: 'Full Stack Software Engineering',
-                    place: 'Portland, OR'
+                    place: 'Portland, OR',
+                    description: '5-6 months of 60-70 hour work weeks learning Full Stack Javascript'
+                }
+            ],
+            volunteering: [
+                {
+                    id: 'FG',
+                    company: 'Free Geek',
+                    place: 'Portland, OR',
+                    description: 'A non-profit organization where volunteers and staff recycle and refurbish old computers, while also learning the hardware components'
                 }
             ]
         }
     },
     methods: {
-        // collapsible() {
-        //     return true;
-        // }
+        collapsible(event) {
+            console.log(event.currentTarget.id, ' was clicked');
+            if(!this.clicked) this.clicked = true;
+            else this.clicked = false;
+        }
     }
 }
 </script>
