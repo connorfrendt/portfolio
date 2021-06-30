2a5ccac2d961c034ada4b4b638292cf540ffe352
Connor Frendt <cjacksonfx@yahoo.com>
2021-06-30 14:16:40 -0700

    About Me wip

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,16 +1,6 @@
 <template>
     <div>
-            <ul>
-                <li>Outdoors</li>
-                <li>Martial arts, gymnastics, dance</li>
-                <li>Video Games</li>
-                <li>Technology</li>
-                <li></li>
-            </ul>
-
-            <div>
-                Hey there!  When I'm not coding, I love spending my time being active, playing music, and video games!  Some of the biggest sports that I do are martial arts, gymnastics, dance, snowboarding and wakeboarding
-            </div>
+            About Me (PH)
     </div>
 </template>
 
